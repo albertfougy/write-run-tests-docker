e3504c9de25e52e3cf57d0b7bd1612acaecbb179
Albert Fougy <albert@fougy.com>
2019-04-04 20:02:21 -0400

    updated all test units

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,2 +1,9 @@
+from europython.factories import ArticleFactory
+
 def test_get_articles(client, db):
-    raise NotImplementedError
+    articles = ArticleFactory.create_batch(3)
+    response = client.get('/api/articles/').json
+
+    for actual, expected in zip(response, articles):
+        assert actual['id'] == expected.id
+        assert actual['title'] == expected.title
